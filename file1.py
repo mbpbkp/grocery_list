ac96469f01bd92798b4e2718a385deb558a14ab5
mbpbkp <mbp.bkp@gmail.com>
2017-06-03 21:06:32 -0400

    commit 2; line 8

--- file1.py
+++ file1.py
@@ -2,4 +2,7 @@
 
 print "i'm learning now!"
 #added file to git. lets see how wthis goes.
-#save without adding to git
+#save without adding to git -- adding this after commit 1
+
+#this lis new stuff. after commit 1
+#
